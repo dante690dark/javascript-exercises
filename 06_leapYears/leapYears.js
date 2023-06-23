38cdb7cccb84c4f7061b79f793edf8a7e67eaa7b
Alexander Manns <dante690dark@gmail.com>
2023-06-23 16:43:29 -0500

    leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
+leapYears(2000);
 // Do not edit below this line
 module.exports = leapYears;

e421b149a5bf5dfc27151cdf673f3045d6a59e04
Alexander Manns <dante690dark@gmail.com>
2023-06-23 16:35:46 -0500

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (param1, param2) {
+  if (
+    param2 < 0 ||
+    param1 < 0 ||
+    typeof param1 !== "number" ||
+    typeof param2 !== "number"
+  ) {
+    return "ERROR";
+  }
+  const greater = Math.max(param1, param2),
+    less = Math.min(param1, param2);
+  let sum = less;
 
+  for (let i = greater; i > less; i--) {
+    sum += i;
+  }
+
+  return sum;
 };
 
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;

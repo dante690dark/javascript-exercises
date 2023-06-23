fb90ae3c8d16736c80f149143b666c79e34abe3d
Alexander Manns <dante690dark@gmail.com>
2023-06-23 00:15:57 -0500

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,6 @@ const reverseString = function (str) {
   return str.split("").reverse().join("");
 };
 
-reverseString("hello");
+reverseString("hello world");
 // Do not edit below this line
 module.exports = reverseString;

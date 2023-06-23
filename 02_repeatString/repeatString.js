fb5a6b63f307a0426fbd95e6fc71972f78120da2
Alexander Manns <dante690dark@gmail.com>
2023-06-22 23:40:13 -0500

    repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, number = Math.floor(Math.random() * 1000)) {
+  let newStr = "";
 
+  if (!str || !number) return "";
+  if (number == -1) return "ERROR";
+
+  for (let i = 0; i < number; i++) {
+    newStr += str;
+  }
+
+  return newStr;
 };
 
+repeatString("hello", "");
 // Do not edit below this line
 module.exports = repeatString;

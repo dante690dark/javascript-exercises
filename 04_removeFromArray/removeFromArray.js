cfecd956d970e61d28647bfbcecf6c1ced7716fe
Alexander Manns <dante690dark@gmail.com>
2023-06-23 13:36:47 -0500

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  args.forEach((e) => {
+    const index = arr.indexOf(e);
+    if (index > -1) arr.splice(index, 1);
+  });
 
+  return arr;
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;

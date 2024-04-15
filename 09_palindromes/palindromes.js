411ac57028570672284103c7fd3da18631785be8
Alexander Manns <dante690dark@gmail.com>
2024-04-15 16:08:40 -0500

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const convertedStr = str.split("").filter(e => e.match(/\w/)).map(e => e.toLowerCase()).join(""),
+          reversedStr = str.split("").filter(e => e.match(/\w/)).map(e => e.toLowerCase()).reverse().join("")
+ 
+    return convertedStr === reversedStr;
 };
 
 // Do not edit below this line

350608afbe2dcabe49473ffdd696037b9636dbff
Alexander Manns <dante690dark@gmail.com>
2024-04-16 00:03:51 -0500

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const serie = function(n) {
+  return n <= 1 ? n : serie(n - 1) + serie(n - 2);
+}
 
+const fibonacci = function(num) {
+  if(num < 0) return 'OOPS'
+
+  const arr = []
+
+  for (let i = 1; i <= num; i++) {
+      arr.push(serie(i))
+  }
+
+  return Math.max(...arr)
 };
 
 // Do not edit below this line

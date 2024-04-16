2b8a22eb1b7c742cd52cdc42720e1c21647f355c
Alexander Manns <dante690dark@gmail.com>
2024-04-16 11:14:40 -0500

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+	let maxAge = 0
+
+	const oldest = people.reduce((obj, person) => {
+
+		const {yearOfBirth, yearOfDeath, name} = person
+
+		const age = yearOfDeath - yearOfBirth || new Date().getFullYear() - yearOfBirth;
+		
+		if(maxAge < age){
+			maxAge = age
+			obj.name = name
+		}
+		
+		return obj
+	}, {})
+
+  return oldest
 };
 
 // Do not edit below this line
